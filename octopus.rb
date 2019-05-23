733c135826633e8c52d054aa0dc2f19ab3a89b4d
the3rdheat <rockstrongo22@gmail.com>
2019-05-22 21:18:32 -0700

    Finished HW

--- octopus.rb
+++ octopus.rb
@@ -9,12 +9,24 @@ def find_n(ar)
     longest
 end
 
-
-
-
 def find_n_squared2(ar)
-    p ar.sort{|a,b|a.length-b.length}[-1]
+    return ar.sort{|a,b|a.length-b.length}[-1]
 end
 
 
-p find_n_squared2(ar)
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+def slow_dance(dir,ar)
+    ar.each_with_index do |el,ind|
+        return ind if el==dir
+    end
+end
+
+
+new_tiles_array = Hash[["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ].map.with_index{|el,ind|[el,ind]}]
+def fast_dance(dir, new_tiles_data_structure)
+    return new_tiles_data_structure[dir] 
+end
+
+p fast_dance("up",new_tiles_array)
+p fast_dance("right-down",new_tiles_array)
